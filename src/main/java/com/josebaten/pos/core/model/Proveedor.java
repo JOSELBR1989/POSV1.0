04caa21b530698bb25637b40ca64dfa5ce6865a3
programacion <programacion@LABC29-29.fkinal.edu>
2019-03-30 22:25:59 -0600

    proveedor.

---
+++ src/main/java/com/josebaten/pos/core/model/Proveedor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.josebaten.pos.core.model;
+
+/**
+ *
+ * @author programacion
+ */
+public class Proveedor {
+    
+}
