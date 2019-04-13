51c43282a1fd16b54cfdfe69f1c246c60944943c
programacion <programacion@LABC29-29.fkinal.edu>
2019-04-13 13:54:55 -0600

    clase

--- src/main/java/com/josebaten/pos/core/MainApp.java
+++ src/main/java/com/josebaten/pos/core/MainApp.java
@@ -1,27 +1,52 @@
 package com.josebaten.pos.core;
 
+import java.io.IOException;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
 public class MainApp extends Application {
-
+    private final String PAQUETE_VISTA = "/com/josebaten/pos/core/view";
+    private Stage escenarioPrincipal; 
+    
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage escenarioPrincipal) throws Exception {
+        this.escenarioPrincipal = escenarioPrincipal; 
+        
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
         
+        
         Scene scene = new Scene(root);
+
         scene.getStylesheets().add("/styles/Styles.css");
         
-        stage.setTitle("JavaFX and Maven");
-        stage.setScene(scene);
-        stage.show();
+        this.escenarioPrincipal.setTitle("JavaFX and Maven");
+        this.escenarioPrincipal.setScene(scene);
+        this.escenarioPrincipal.show();
     }
 
+    public void mostrarVentanaPrincipal() throws IOException{
+        
+    }
+    
+    public Initializable cambiarEscena(String fxml, int ancho, int alto) throws IOException{
+        Initializable resultado = null;
+        
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResource(PAQUETE_VISTA.concat("Scene.fxml") ));
+        Scene escena = new Scene(root, ancho,alto);
+        this.escenarioPrincipal.setScene(escena);
+        this.escenarioPrincipal.sizeToScene();
+        
+        return resultado;
+    }
+    
+    
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
      * main() serves only as fallback in case the application can not be
